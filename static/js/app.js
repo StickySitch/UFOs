5eb1b0db49ef51d6a1eb91105434edcaa2f6f1d4
Jack Jones <fuzzydunlop19@gmail.com>
2022-01-01 22:55:33 -0800

    Dark theme, table and filtering complete.

--- static/js/app.js
+++ static/js/app.js
@@ -5,20 +5,21 @@ const tableData = data;
 var tbody = d3.select("tbody");
 
 function buildTable(data) {
-    // clearing existing data
+    // First, clear out any existing data
     tbody.html("");
-
-    // loop through each object in the data
+  
+    // Next, loop through each object in the data
     // and append a row and cells for each value in the row
     data.forEach((dataRow) => {
-        // Append a row to the table body
-        let row = tbody.append("tr");
-        // Loop through each field in the dataRow and add
-        // each value as a table cell (td)
-        Object.values(dataRow).forEach((val) => {
-            let cell = row.append("td");
-            cell.text(val);
-        });
+      // Append a row to the table body
+      let row = tbody.append("tr");
+  
+      // Loop through each field in the dataRow and add
+      // each value as a table cell (td)
+      Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+        cell.text(val);
+      });
     });
 }
 
@@ -29,14 +30,14 @@ function handleClick() {
     // Check to see if a date was entered and filter the
     // data using that date.
     if (date) {
-        // Apply `filter` to the table data to only keep the
-        // rows where the `datetime` value matches the filter value
-        filteredData = filteredData.filter(row => row.dateTime === date);
-    };
+      // Apply `filter` to the table data to only keep the
+      // rows where the `datetime` value matches the filter value
+      filteredData = filteredData.filter(row => row.datetime === date);
+    }
     // Rebuild the table using the filtered data
     // @NOTE: If no date was entered, then filteredData will
     // just be the original tableData.
-    buildTable(filteredData);
+  buildTable(filteredData);
 }
 // Attach an event to listen for the form button
 d3.selectAll("#filter-btn").on("click", handleClick);
